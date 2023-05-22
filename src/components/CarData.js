305258f9ff4d63eb0ab5c8e49faeb992373dc870
hariharasudhan <ssudhan048@gmail.com>
2023-05-22 10:06:23 +0530

    first commit

---
+++ src/components/CarData.js
@@ -0,0 +1,93 @@
+import Golf6 from "../images/cars-big/golf6.jpg";
+import AudiA1 from "../images/cars-big/audia1.jpg";
+import Toyota from "../images/cars-big/toyotacamry.jpg";
+import Bmw320 from "../images/cars-big/bmw320.jpg";
+import Benz from "../images/cars-big/benz.jpg";
+import Passat from "../images/cars-big/passatcc.jpg";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "Mahindra",
+      price: "15k",
+      img: Golf6,
+      model: "Mahindra",
+      mark: "XUV300",
+      year: "2019",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Audi A1 S-Line",
+      price: "25k",
+      img: AudiA1,
+      model: "Audi",
+      mark: "A1",
+      year: "2012",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Toyota Fortuner",
+      price: "14k",
+      img: Toyota,
+      model: "Fortuner",
+      mark: "Toyota",
+      year: "2022",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Hybrid",
+    },
+  ],
+  [
+    {
+      name: "BMW THE3 ModernLine",
+      price: "30k",
+      img: Bmw320,
+      model: "THE3",
+      mark: "BMW",
+      year: "2020",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Mercedes-Benz E",
+      price: "25k",
+      img: Benz,
+      model: "Benz E ",
+      mark: "Mercedes",
+      year: "2022",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "VW Passat CC",
+      price: "17k",
+      img: Passat,
+      model: "Passat CC",
+      mark: "Volkswagen",
+      year: "2015",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+];
